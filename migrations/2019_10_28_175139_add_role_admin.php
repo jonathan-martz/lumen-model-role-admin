7bfe3f92e04acf099cf936a8a6c4402bc35e8914
jonathan-martz <github@jmartz.de>
2019-12-27 18:38:14 +0100

    fix migration class name

--- migrations/2019_10_28_175139_add_role_admin.php
+++ migrations/2019_10_28_175139_add_role_admin.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class AddRoleAdminTable extends Migration
+class AddRoleAdmin extends Migration
 {
     /**
      * Run the migrations.
